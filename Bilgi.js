08e1e7fb2054c9a4ec445a889adf83114050ca72
SalihKalender <m.salihk72@gmail.com>
2020-12-11 18:02:21 +0300

    Delete Bilgi.js

--- Bilgi.js
+++
@@ -1,9 +0,0 @@
-/*
-    * :hover olab birşeyin geri gelirkende transition'la olması icin hem :hover'a hemde normal element'e transition ver
-
-    * Dinamik img'ler icin linkleri require() ==> icine yazman lazım  instance'da
-    
-    * this.Answer = choise; ===> choise'yi this.answer'a atar
-
-    * Eger degeri sonradan girilecek olan bir degiskene object gireceksen o degiskeni basta null degil object yap
-*/
